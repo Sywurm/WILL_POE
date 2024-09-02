9e47dad1de6817c433ed70519e0bc8899b3b1db9
Malcom Thonger <malcozui@gmail.com>
2024-09-02 20:03:23 +0200

    Dialogue System pt2
    Moved dialogue to correct scene, and added options to make dialogue require any given gameobject to be active before proceeding, allowing for checking that the player has opened a specified menu

--- Assets/Scripts/Tutorial/DialogueSystem.cs
+++ Assets/Scripts/Tutorial/DialogueSystem.cs
@@ -1,24 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+#nullable enable annotations
 
+[System.Serializable]
+public class DialoguePiece
+{
+    public string MainTextContents;
+    public bool requireGivenGameObjectActive;
+    public GameObject? activePassCheck;
+}
 public class DialogueSystem : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI textComponent;
-    public List<string> dialogues = new();
-    private int currentDialogueSelected = 0;
+    [SerializeField] private List<DialoguePiece> dialogueItems = new();
+    private Image panelImg;
+    private int curDialogueIdx = 0;
 
-    private void Awake()
+    private void Start()
     {
-        textComponent.text = dialogues[currentDialogueSelected];
+        panelImg = GetComponent<Image>();
+        textComponent.text = dialogueItems[curDialogueIdx].MainTextContents;
     }
 
     public void OnProceedButtonPress()
     {
-        currentDialogueSelected++;
-        if (currentDialogueSelected < dialogues.Count)
-            textComponent.text = dialogues[currentDialogueSelected];
+        if (curDialogueIdx + 1 >= dialogueItems.Count)
+        {
+            HideTutorial(); // Last item passed, tutorial can be marked as complete and the object deactivated
+            return;
+        }
+
+        if (dialogueItems[curDialogueIdx].requireGivenGameObjectActive)
+        {
+            if (dialogueItems[curDialogueIdx].activePassCheck == null || !dialogueItems[curDialogueIdx].activePassCheck.activeInHierarchy)
+                return;
+
+            textComponent.text = dialogueItems[curDialogueIdx + 1].MainTextContents;
+        }
+        else
+        {
+            Debug.Log("B");
+            textComponent.text = dialogueItems[curDialogueIdx + 1].MainTextContents;
+        }
+        panelImg.raycastTarget = !dialogueItems[curDialogueIdx + 1].requireGivenGameObjectActive;
+        curDialogueIdx++;
     }
 
 
