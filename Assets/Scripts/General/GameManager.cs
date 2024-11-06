760c4cc02c43e5d5d853c8600240a85aeb4e7d4b
Maxime DeClerk <maximedeclerck104@gmail.com>
2024-11-06 14:04:54 +0200

    GameManager Time Set up
    Seconds = 30
    Hours = 8
    Days = 5
    Pause Functionality works
    Full Day loop = 4 min
    Full Week Loop = 20 min

--- Assets/Scripts/General/GameManager.cs
+++ Assets/Scripts/General/GameManager.cs
@@ -1,20 +1,11 @@
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     #region Variables
     #region OfficeStates
-    //This is a list of employees, this list will be updated by the emplyoeeManager as a new emploee is hired or fired
-    private List<GameObject> listOfActiveEmployees;
-
-    //These Variables are the current office stats that will be seen on the UI.
-    [Tooltip("This Value Represents the Office happiness. It can only be reAssigned from within this script ")]
-    [SerializeField] private float OfficeHappitness;
-
-    [Tooltip("This Value Represents the Office Efficiency. It can only be reAssigned from within this script ")]
-    [SerializeField] private float OfficeEfficiency;
-
     [Tooltip("This Value Represents the Office State. It can only be reAssigned from within this script ")]
     [SerializeField] private float OfficeState;
 
@@ -32,8 +23,25 @@ public class GameManager : MonoBehaviour
     [SerializeField][Range(0,60)] private float seconds = 0f;
     [SerializeField][Range(0, 60)] private float currentTime = 0f;
     [SerializeField] private float multiplier = 1f;
-    [SerializeField][Range(0, 24)] private int hour = 00;
-    [SerializeField][Range(0, 5)] private int day = 0;
+    [SerializeField][Range(8, 17)] private int hour = 8;
+    [SerializeField] private int minuets = 00;
+    [SerializeField][Range(1, 5)] private int day = 0;
+
+    [SerializeField] bool timeIsPaused = false;
+
+    [SerializeField] TextMeshProUGUI TimeTMP;
+    [SerializeField] TextMeshProUGUI HourTMP;
+    [SerializeField] TextMeshProUGUI DayTMP;
+
+    private enum DayOfWeek
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+    }
+    private DayOfWeek dayOfWeek;
 
     #endregion
 
@@ -43,13 +51,31 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        timeIsPaused = false;
     }
-
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Debug.Log("Space Pressed");
+            if (timeIsPaused == false)
+            {
+                Debug.Log("Time Paused");
+                timeIsPaused = true;
+            }
+            else if (timeIsPaused == true)
+            {
+                Debug.Log("Time unPaused");
+                timeIsPaused = false;
+            }
+        }
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
         TimeManager();
+
+        
     }
     #endregion
 
@@ -59,34 +85,70 @@ public class GameManager : MonoBehaviour
 
     private void TimeManager()
     {
-        currentTime = seconds * multiplier;
-        seconds += Time.fixedDeltaTime;
+        if (timeIsPaused == false)
+        {
+            currentTime = seconds * multiplier;
+            seconds += Time.fixedDeltaTime;
+        }
+        else
+        {
+            
+        }
 
-        if(currentTime >= 60f)
+
+        if(currentTime >= 30f)
         {
             seconds = 0f;
             currentTime = 0f;
 
-            hour++;
+            hour +=1;
         }
 
-        if(hour >= 24 )
+        if(currentTime >= 16 )
         {
-            hour = 0;
-            day++;
+            minuets = 30;
         }
+        else 
+        {
+            minuets = 00;
+        }
+
+        if(hour >= 17 )
+        {
+            hour = 8;
+            day += 1;
+        }
+
+        switch (day)
+        {
+            case 1:
+                dayOfWeek = DayOfWeek.Monday;
+                break;
+            case 2:
+                dayOfWeek = DayOfWeek.Tuesday;
+                break;
+            case 3:
+                dayOfWeek = DayOfWeek.Wednesday;
+                break;
+            case 4:
+                dayOfWeek = DayOfWeek.Thursday;
+                break;
+            case 5:
+                dayOfWeek = DayOfWeek.Friday;
+                break;
+                default:
+                dayOfWeek = DayOfWeek.Monday;
+                break;
+
+        }
+
+        TimeTMP.text = ((int)currentTime).ToString();
+        HourTMP.text = $"{(int)hour} : {(minuets).ToString("D2")}";
+        DayTMP.text = dayOfWeek.ToString();
     }
+
 
     #endregion
-
-    private void OfficeStats()
-    {
-        //Setting these States so that they can be used by the UI elements.
-        _OfficeHappitness = OfficeHappitness;
-        _OfficeEfficiency = OfficeEfficiency;
-        _OfficeState = OfficeState;
-    }
-
 
     #endregion
 
